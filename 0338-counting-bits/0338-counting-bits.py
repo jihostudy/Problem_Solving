9186cce6e1583aa63bdbf4216bc6ea3bd0447fc4
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-03-09 20:02:05 +0900

    Time: 107 ms (18.55%), Space: 23.4 MB (31.47%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,11 +1,24 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        for elmt in range(n+1):          
-          tmp = 0          
-          while(elmt != 0):            
-            if(elmt % 2 != 0):
-              tmp += 1
-            elmt //= 2
-          ans.append(tmp)        
+      ans = None
+      if(n == 0):
+        ans = [0]
+        return ans
+      elif(n == 1):
+        ans = [0,1]
+        return ans
+      elif(n >= 2):
+        ans = [0,1]
+        구간값 = 2
+        cnt = 0
+        for elmt in range(2,n+1):          
+          if(구간값 <= cnt):
+            구간값 *= 2
+            cnt = 0
+          print(구간값, elmt, cnt)
+          if(elmt - 구간값 < 구간값 // 2):                    
+            ans.append(ans[elmt - (구간값 // 2)])
+          else:            
+            ans.append(ans[elmt - (구간값 // 2)] + 1)
+          cnt += 1          
         return ans
