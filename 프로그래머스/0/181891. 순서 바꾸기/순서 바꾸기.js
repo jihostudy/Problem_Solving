c2f23a85c554f1973bc6f9e14ab575552d185802
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-05-22 02:24:57 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,24 +1,4 @@
 function solution(num_list, n) {
-    var answer = [];
-    
-    const left = [];
-    const right = [];
-    
-    for(let i = 0; i < num_list.length; i++) {
-        if(i < n){
-            left.push(num_list[i]);
-        }
-        else {
-            right.push(num_list[i]);
-        }
-    }
-    
-    
-    for(let i = 0; i < right.length; i++){
-        answer.push(right[i]);
-    }
-    for(let i = 0; i < left.length; i++){
-        answer.push(left[i]);
-    }
-    return answer;
+    num_list.push(...num_list.splice(0,n));
+    return num_list;
 }
