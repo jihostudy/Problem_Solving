e5b0860514fafa3888bdf893c71aa3edf0b8a0ac
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-06-06 19:58:23 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,8 +1,4 @@
-function solution(my_string, index_list) {
-    var answer = '';
-    const arrlen = index_list.length;
-    for (let i = 0; i < arrlen; i++){
-        answer += my_string[index_list[i]]
-    }
-    return answer;
+function solution(my_string, index_list) {   
+     const arr = index_list.map((val) => my_string[val]).join("")
+    return arr;
 }
