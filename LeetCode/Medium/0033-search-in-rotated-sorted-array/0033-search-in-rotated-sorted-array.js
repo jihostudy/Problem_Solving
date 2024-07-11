7a6ef85c10ec8ced108c675a62af134bee4cbeef
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-07-11 16:52:55 +0900

    Time: 52 ms (64.28%), Space: 49 MB (38.20%) - LeetHub

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -1,17 +1,24 @@
+// Without sorting
 var search = function (nums, target) {
-  const arr = nums.toSorted((a, b) => a - b);
-  // console.log(arr);
-  let [left, right] = [0, arr.length - 1];
+  const len = nums.length;
+  let [left, right] = [0, len - 1];
+
   while (left <= right) {
-    // console.log("left: ", left, "right: ", right);
     const mid = Math.floor((left + right) / 2);
-    if (arr[mid] < target) {
-      left = mid + 1;
-    } else if (arr[mid] > target) {
-      right = mid - 1;
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target <= nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
     } else {
-      // console.log("Found mid: ", mid);
-      return nums.indexOf(target);
+      if (nums[mid] <= target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
   }
   return -1;
