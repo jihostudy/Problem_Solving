fc05081819f388c381e640e21113359ba24d8eb3
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-07-19 01:16:00 +0900

    Time: 52 ms (63.32%), Space: 49.3 MB (20.53%) - LeetHub

--- LeetCode/Easy/0100-same-tree/0100-same-tree.js
+++ LeetCode/Easy/0100-same-tree/0100-same-tree.js
@@ -1,18 +1,13 @@
 // p,q are roots of two binary trees
 var isSameTree = function (p, q) {
-  console.log("Checking p,q", p, q);
   // 두개다 null인 경우
   if (p === null && q === null) {
-    console.log("p,q 둘다 null이라서 true");
     return true;
   }
   // 둘다 null이 아닌 경우
   else if (p !== null && q !== null) {
-    if (p.val !== q.val) {
-      console.log("p,q 둘다 null이 아니고 값이 같지 않아서 false");
-      return false;
-    }
-    console.log("p,q 둘다 null이 아니고 값이 같아서 다음값 판정");
+    if (p.val !== q.val) return false;
+
     if (
       (p.left !== null && q.left === null) ||
       (p.left === null && q.left !== null)
