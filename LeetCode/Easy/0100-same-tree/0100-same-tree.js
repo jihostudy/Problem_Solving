ad73434e1b05df82198f1eb0f93250d8c49708c3
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-07-19 01:16:50 +0900

    Time: 49 ms (78.14%), Space: 48.6 MB (93.72%) - LeetHub

--- LeetCode/Easy/0100-same-tree/0100-same-tree.js
+++ LeetCode/Easy/0100-same-tree/0100-same-tree.js
@@ -1,29 +1,15 @@
 // p,q are roots of two binary trees
 var isSameTree = function (p, q) {
-  // 두개다 null인 경우
-  if (p === null && q === null) {
+  // #1. p and q are null
+  if (!p && !q) {
     return true;
   }
-  // 둘다 null이 아닌 경우
-  else if (p !== null && q !== null) {
-    if (p.val !== q.val) return false;
-
-    if (
-      (p.left !== null && q.left === null) ||
-      (p.left === null && q.left !== null)
-    ) {
-      return false;
-    }
-    if (
-      (p.right !== null && q.right === null) ||
-      (p.right === null && q.right !== null)
-    ) {
-      return false;
-    }
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  // #2. p and q should not have null + same value
+  else if (!p || !q || p.val !== q.val) {
+    return false;    
   }
-  // 둘중 하나만 null인 경우
+  // #3. Check left and right validation
   else {
-    return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
   }
 };
