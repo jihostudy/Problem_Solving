6b6146f039bba100ce282e4fd6d4f70f06c06b95
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-07-22 20:42:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 996 ms, Memory: 120308 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -1,33 +1,41 @@
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
-const [N, M] = input.shift().split(' ').map(Number); // [정점의 개수, 간선의 개수]
-const arr = input.map((v) => v.split(' ').map(Number));
-let visited = Array(N + 1).fill(false);
-let graph = [...Array(N + 1)].map(() => []);
-let answer = 0;
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../input.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
-arr.map(([from, to]) => {
+const [N, M] = input.shift().split(" ").map(Number);
+let graph = [...Array(N + 1)].map(() => []);
+const visited = new Array(N + 1).fill(false);
+const arr = input.map((v) => v.split(" ").map(Number));
+// # 입력 처리
+
+// 양방향(무방향) 그래프로 만들기
+arr.forEach(([from, to]) => {
   graph[from].push(to);
   graph[to].push(from);
 });
 
-const bfs = (start) => {
-  let queue = [start];
+const BFS_FindConnected = (node) => {
+  const queue = [node];
+  visited[node] = true;
   while (queue.length) {
-    const cur = queue.shift();
-    for (const vertax of graph[cur]) {
-      if (!visited[vertax]) {
-        visited[vertax] = true;
-        queue.push(vertax);
+    const val = queue.shift();
+    for (const n of graph[val]) {
+      if (!visited[n]) {
+        visited[n] = true;
+        queue.push(n);
       }
     }
   }
 };
 
-for (let i = 1; i <= N; i++) {
+let answer = 0;
+for (i = 1; i <= N; i++) {
   if (!visited[i]) {
-    bfs(i);
     answer++;
+    BFS_FindConnected(i);
   }
 }
 console.log(answer);
