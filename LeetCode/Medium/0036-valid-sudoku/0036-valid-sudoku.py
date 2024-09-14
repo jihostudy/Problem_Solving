901242fc69e25b99559eff846dc5af1403c23ed1
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-09-14 13:40:42 +0900

    Time: 137 ms (5.03%), Space: 11.7 MB (71.51%) - LeetHub

---
+++ LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    # 배열이 1~9 하나씩만 가지고 있는지 판단하는 코드
+    def isValid(self, array):
+        s = set()
+        for num in array:
+            if(num != "."):
+                if num not in s:
+                    s.add(num)
+                else:
+                    return False
+        return True
+
+
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        LENGTH = 9
+        #1. 가로 배열이 Valid한지 판단
+        for row in range(LENGTH):
+            if self.isValid(board[row]) == False:
+                return False
+        #2. 세로 배열이 Valid한지 판단
+        for col in range(LENGTH):
+            arr = []
+            for row in range(LENGTH):
+                arr.append(board[row][col])
+            if self.isValid(arr) == False:
+                return False
+        #3. 3 * 3 배열이 Valid 한지 판단
+        i, j = 0, 0
+        while(i < 3):
+            arr = []
+            for row in range(3*i, 3*i+3):
+                for col in range(3*j, 3*j+3):
+                    arr.append(board[row][col])
+            if self.isValid(arr) == False:
+                return False
+            j += 1
+            if(j==3):
+                i += 1
+                j = 0
+        return True
