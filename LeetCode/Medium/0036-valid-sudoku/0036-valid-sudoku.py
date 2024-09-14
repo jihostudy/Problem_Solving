aa624a2b34ba2458ad265abb65d75fc54cbb6150
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-09-14 13:45:57 +0900

    Time: 140 ms (5.03%), Space: 11.7 MB (71.51%) - LeetHub

--- LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku.py
+++ LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku.py
@@ -19,22 +19,18 @@ class Solution(object):
         LENGTH = 9
         #1. 가로 배열이 Valid한지 판단
         for row in range(LENGTH):
-            if self.isValid(board[row]) == False:
+            arr = board[row]
+            if self.isValid(arr) == False:
                 return False
         #2. 세로 배열이 Valid한지 판단
         for col in range(LENGTH):
-            arr = []
-            for row in range(LENGTH):
-                arr.append(board[row][col])
+            arr = [board[row][col] for row in range(LENGTH)]
             if self.isValid(arr) == False:
                 return False
         #3. 3 * 3 배열이 Valid 한지 판단
         i, j = 0, 0
         while(i < 3):
-            arr = []
-            for row in range(3*i, 3*i+3):
-                for col in range(3*j, 3*j+3):
-                    arr.append(board[row][col])
+            arr = [board[row][col] for row in range(3*i, 3*(i+1)) for col in range (3*j, 3*(j+1))]
             if self.isValid(arr) == False:
                 return False
             j += 1
