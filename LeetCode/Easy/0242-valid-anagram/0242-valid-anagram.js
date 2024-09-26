650a3543b7e9e62b9965d9335a9ace8ae42cf8e3
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-09-27 02:30:17 +0900

    Time: 81 ms (26.80%), Space: 54.9 MB (12.11%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.js
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.js
@@ -4,27 +4,17 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  // #1. 저장 배열
-  const arr = new Array(26).fill(0);
-  const charstr = s.split("");
+  const [sArr, tArr] = [s.split(""), t.split("")];
+  sArr.sort();
+  tArr.sort();
 
-  for (const char of charstr) {
-    const idx = char.charCodeAt() - 97;
-    arr[idx] += 1;
-  }
-  const inputstr = t.split("");
-  for (const char of inputstr) {
-    const idx = char.charCodeAt() - 97;
-    if (arr[idx] > 0) {
-      arr[idx] -= 1;
-    } else {
+  const len = sArr.length;
+  if (sArr.length !== tArr.length) return false;
+  for (let i = 0; i < len; i++) {
+    if (sArr[i] != tArr[i]) {
       return false;
     }
   }
-  // #2. 남아있는거 있음?
-  let i = 0;
-  for (i; i < 26; i++) {
-    if (arr[i] != 0) return false;
-  }
   return true;
 };
+
