05bf2fcb7929b2ae65770bb58fc0f23a05b4d09b
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2025-07-13 15:08:46 +0900

    Time: 0 ms (100%), Space: 54.9 MB (5.54%) - LeetHub

--- LeetCode/0198-house-robber/0198-house-robber.js
+++ LeetCode/0198-house-robber/0198-house-robber.js
@@ -7,16 +7,15 @@ var rob = function (nums) {
 
   const arr = new Array(N);
 
-  if (N == 1) {
-    return nums[0];
+  if (N <= 2) {
+    return Math.max(...nums);
   }
   arr[0] = nums[0];
   arr[1] = Math.max(nums[0], nums[1]);
-
-  for (let i = 2; i < N; i++) {
+  let i = 2;
+  for (i; i < N; i++) {
     arr[i] = Math.max(arr[i - 2] + nums[i], arr[i - 1]);
   }
-  // console.log(arr);
 
   return arr[N - 1];
 };
