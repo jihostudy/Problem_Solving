db7d3bcad7e847db45edfe266fc331523bbd837a
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2025-08-06 17:54:12 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9332 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : "../../input.txt";
+const inputs = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+
+const L = Number(inputs[0]);
+const values = inputs[1].split("");
+
+// console.log(L);
+// console.log(values);
+
+/**
+ * L : 31
+ * mod : 1234567891
+ * ai * r**(i) for i in (0 ~ L-1)
+ */
+
+/**
+ * r**(i)를 계산한 dp 배열 사전에 만들기 (길이 : L)
+ *
+ * 각 value of values에 대해
+ * 1. ai -> 값으로 변환하기
+ * ai.charCodeAt(0) - 96
+ *
+ * 2. 각 값을 더하기
+ * 3. mod로 나누기
+ */
+const R = 31;
+dp = new Array(L).fill(1);
+for (let i = 1; i < L; i++) {
+  dp[i] = dp[i - 1] * R;
+}
+// console.log("dp: ", dp);
+
+let answer = 0;
+
+const MOD = 1234567891;
+for (const [index, value] of values.entries()) {
+  const a = value.charCodeAt(0) - 96;
+  answer += a * dp[index];
+  // console.log("index:", index, "adding : ", answer);
+  answer %= MOD;
+}
+console.log(answer);
