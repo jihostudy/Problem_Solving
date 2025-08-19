f6f9829a4b1dbe86f9f4c6625c4787b7e79db9f4
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2025-08-19 15:52:42 +0900

    [Silver II] Title: 과일 탕후루, Time: 172 ms, Memory: 15236 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.js
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.js
@@ -2,35 +2,37 @@
 const fs = require("fs");
 const filePath =
   process.platform === "linux" ? "/dev/stdin" : "../../input.txt";
-const input = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+const inputs = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let arr = input[1].split(" ").map((n) => Number(n));
-let max = 0; // 최대 과일 갯수를 담을 변수
+const N = Number(inputs[0]);
+const numbers = inputs[1].split(" ").map(Number);
+
+let answer = 0;
+let [left, right] = [0, 0];
+
 let group = Array(10).fill(0); // group [i] 는 현재 그룹에서 i번 과일이 몇개 있는지 나타냄
-let left = 0; // 왼쪽 idx
-let right = 0; // 오른쪽 idx
 let count = 0; // 과일 갯수
 let kind = 0; // 과일 종류
-while (right < arr.length) {
-  if (group[arr[right]] === 0) {
+
+while (right < N) {
+  if (group[numbers[right]] === 0) {
     kind += 1;
   }
-  group[arr[right]] += 1;
+  group[numbers[right]] += 1;
   ++count;
-  if (kind > 2) {
-    // kind 2 될때까지 left를 올림
 
-    while (kind > 2) {
-      group[arr[left]] -= 1;
-      if (group[arr[left]] === 0) {
-        kind -= 1;
-      }
-      count -= 1;
-      ++left;
+  // kind 2 될때까지 left를 올림
+  while (kind > 2) {
+    group[numbers[left]] -= 1;
+    if (group[numbers[left]] === 0) {
+      kind -= 1;
     }
+    count -= 1;
+    ++left;
   }
-  max = Math.max(max, count);
+
+  answer = Math.max(answer, count);
   ++right;
 }
 
-console.log(max);
+console.log(answer);
