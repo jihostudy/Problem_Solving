cfdd9c261728885b4594459827d95fac5d5342e0
JiHo00 <cale.jiho@gmail.com>
2026-02-25 23:41:47 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,25 +1,15 @@
-# https://www.acmicpc.net/problem/25206
-# 너의 평점은, 실버5
+score = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 
+         'B0': 3.0, 'C+': 2.5, 'C0': 2.0,
+         'D+': 1.5, 'D0': 1.0, 'F':0.0}
+average = 0
+totall = 0
 
-scores = {
-  "A+" : 4.5,
-  "A0" : 4.0, 
-  "B+" : 3.5,
-  "B0" : 3.0,
-  "C+" : 2.5,
-  "C0" : 2.0,
-  "D+" : 1.5,
-  "D0" : 1.0,
-  "F" : 0.0,
-}
-
-sum_등급 = 0.0
-sum_학점 = 0.0
-
-for _ in range(20):
-  _, 학점, 등급 = input().split(" ")
-  if(등급 != "P"):
-    sum_등급 += float(학점) * scores[등급]
-    sum_학점 += float(학점)
-print(sum_등급 / sum_학점)
-
+for i in range(20):
+  lecture, credit, grade = input().split() 
+  if grade =='P':
+    continue
+  else:
+    average += float(credit)*score[grade]
+    totall +=float(credit)
+    
+print(average/totall)
